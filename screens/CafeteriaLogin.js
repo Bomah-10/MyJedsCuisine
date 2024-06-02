e87f7d7ed671470f82d84ad837b1a5ce69bc1ae8
Bomah-10 <kashipondokaananias@gmail.com>
2024-06-02 18:12:17 +0200

    Buttons

--- screens/CafeteriaLogin.js
+++ screens/CafeteriaLogin.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginVertical: 10,
     width: 80,
-    height: 40,
+    height: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
